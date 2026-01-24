f89860bca0107f299b5ced023a6c1a6085a45aaa
John <jonathan.plantey@gmail.com>
2026-01-24 02:50:35 +0100

    wip

--- config/services.php
+++ config/services.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the jmonitor/jmonitor-bundle package.
- *
- * (c) Jonathan Plantey <jonathan.plantey@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use Jmonitor\Collector\Apache\ApacheCollector;
